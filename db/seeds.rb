49045338f94afd3bd265e52f3b98a90e55ecd223
pdkproitf <phucdaica.pro@gMAIL.com>
2016-07-07 09:44:17 +0700

    change file seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,43 +6,43 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # seed_file = File.join(Rails.root, 'db', 'seeds.yml')
-# config = YAML::load_file(seed_file)
+config = YAML::load_file(seed_file)
 
-# Category.create(config["categories_list"])
-# Publish.create(config["publishs_list"])
-# Book.create(config["books_list"])
-@category = Array.new
+Category.create(config["categories_list"])
+Publish.create(config["publishs_list"])
+Book.create(config["books_list"])
+# @category = Array.new
 
-12.times do
-  category = FactoryGirl.build(:category)
-  if !Category.find_by_name(category.name)
-    @category.push(Category.create(:name => category.name).id)
-  end
-end
+# 12.times do
+#   category = FactoryGirl.build(:category)
+#   if !Category.find_by_name(category.name)
+#     @category.push(Category.create(:name => category.name).id)
+#   end
+# end
 
-i = 0
-10.times do
-  publish = FactoryGirl.build(:publish)
-  if !Publish.find_by_name(publish.name)
-    @publish = Publish.create(:name => publish.name)
-    10.times do
-      i = 0 if i >= @category.size
-      book = FactoryGirl.build(:book)
-      Book.create(
-                category_id: @category[i],
-                title: book.title,
-                author: book.author,
-                publish_id: @publish.id,
-                cost: book.cost,
-                sale: book.sale,
-                photo: book.photo,
-                content: book.content,
-                weight: book.weight,
-                size: book.size,
-                pages: book.pages,
-                date: book.date
-            )
-      i = i+1
-    end
-  end
-end
+# i = 0
+# 10.times do
+#   publish = FactoryGirl.build(:publish)
+#   if !Publish.find_by_name(publish.name)
+#     @publish = Publish.create(:name => publish.name)
+#     10.times do
+#       i = 0 if i >= @category.size
+#       book = FactoryGirl.build(:book)
+#       Book.create(
+#                 category_id: @category[i],
+#                 title: book.title,
+#                 author: book.author,
+#                 publish_id: @publish.id,
+#                 cost: book.cost,
+#                 sale: book.sale,
+#                 photo: book.photo,
+#                 content: book.content,
+#                 weight: book.weight,
+#                 size: book.size,
+#                 pages: book.pages,
+#                 date: book.date
+#             )
+#       i = i+1
+#     end
+#   end
+# end
