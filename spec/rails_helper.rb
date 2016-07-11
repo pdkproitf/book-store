1b14eda47407a8ad4c31a68ee08bebc4cba5796a
buiquangthang <buiquangthangit@gmail.com>
2016-07-11 09:57:45 +0700

    wwhat do heo

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,12 +31,7 @@ Warden.test_mode!
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -46,8 +41,9 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
   config.include Capybara::DSL
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
-  config.include Warden::Test::Helpers, type: :request
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,22 +1,24 @@
 require 'rails_helper'
-include Warden::Test::Helpers
-Warden.test_mode!
+
 
 describe 'User page' do
 
   subject { page }
   describe "user registration" do
-    before { visit new_user_registration_path }
+    before { visit new_user_regitration_path }
     #let(:submit) { "Sign up" }
 
-
-
+      it "check h2" do
+        within 'h2' do
+            expect(page).to have_content "Sign up"
+        end
+      end
       it "ridirect the previous page" do
         fill_in("Email", with: 'aaaa12@gmail.com', :match => :prefer_exact)
         fill_in("Password", with: 'iloveyou', :match => :prefer_exact)
         fill_in("Password confirmation", with: 'iloveyou', :match => :prefer_exact)
         click_button "Sign up"
-        expect(page).to have_selector("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.")
+        expect(page).to have_content("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.")
     end
 
 
