cbb2cb44996d71064c9fdb686574bc727a8872ca
pdkproitf <phucdaica.pro@gMAIL.com>
2016-07-14 22:10:42 +0700

    test book show details

--- features/book_pages/index.feature
+++ features/book_pages/index.feature
@@ -1,6 +1,11 @@
 Feature: Books page
 
 Scenario: Viewing application's books page
-  Given there're a book titled "My book" image "Myphoto.jpg" with cost 2 And  sale 1
+  Given there're a book titled "My book" photo "Myphoto.jpg" with cost 2 sale 1 category "my category" publish "my publish" author "my authour" content "my content" weight 500 size "50x50" pages 55 date "2016-07-14 21:58:10 +0700"
   When I am on the books
   Then I should see the "My book" image "Myphoto.jpg" with cost 2 and 1 book
+
+Scenario: View'ing details of a book
+  Given there're a book titled "My book" photo "Myphoto.jpg" with cost 2 sale 1 category "my category" publish "my publish" author "my authour" content "my content" weight 500 size "50x50" pages 55 date "2016-07-14 21:58:10 +0700"
+  When I am on the book details
+  Then I should see book titled "My book" image "Myphoto.jpg" with cost 2 sale 1 category "my category" publish "my publish" author "my authour" content "my content" weight 500 size "50x50" date "2016-07-14 21:58:10 +0700"
--- features/step_definitions/book/step_book_index.rb
+++ features/step_definitions/book/step_book_index.rb
@@ -1,9 +1,11 @@
-Given(/^there're a book titled "([^"]*)" image "([^"]*)" with cost (\d+) And  sale (\d+)$/) do |title, photo, cost, sale|
-  @book = FactoryGirl.create(:book, title: title, photo: photo, cost: cost, sale: sale)
+Given(/^there're a book titled "([^"]*)" photo "([^"]*)" with cost (\d+) sale (\d+) category "([^"]*)" publish "([^"]*)" author "([^"]*)" content "([^"]*)" weight (\d+) size "([^"]*)" pages (\d+) date "([^"]*)"$/) do |title, photo, cost, sale, category, publish, author, content, weight, size, pages, date|
+@category = FactoryGirl.create(:category, name: category)
+@publish = FactoryGirl.create(:publish, name: publish)
+@book = FactoryGirl.create(:book, title: title, photo: photo, cost: cost, sale: sale, category_id: @category.id, publish_id: @publish.id, author: author, content: content, weight: weight,size: size, pages: pages, date: DateTime.parse(date).to_date)
 end
 
 When(/^I am on the books$/) do
-  visit "/books"
+  visit books_path
 end
 
 Then(/^I should see the "([^"]*)" image "([^"]*)" with cost (\d+) and (\d+) book$/)  do |title, photo, cost, sale|
@@ -13,4 +15,22 @@ Then(/^I should see the "([^"]*)" image "([^"]*)" with cost (\d+) and (\d+) book
   # expect(page.body).to have_css(".product-f-image")
   expect(page).to have_content((@book.cost - @book.cost * @book.sale).round(2))
   expect(page).to have_content(@book.cost)
+end
+# **************************************************************************
+When(/^I am on the book details$/) do
+  visit book_path(@book)
+end
+
+Then(/^I should see book titled "([^"]*)" image "([^"]*)" with cost (\d+) sale (\d+) category "([^"]*)" publish "([^"]*)" author "([^"]*)" content "([^"]*)" weight (\d+) size "([^"]*)" date "([^"]*)"$/) do |title, photo, cost, sale, category, publish, author, content, weight, size, date|
+  expect(page).to have_content(@book.title)
+  expect(page.body).to include(@book.photo)
+  expect(page).to have_content((@book.cost - @book.cost * @book.sale).round(2))
+  expect(page).to have_content(@book.cost)
+  expect(page).to have_content(@category.name)
+  expect(page).to have_content(@publish.name)
+  expect(page).to have_content(@book.author)
+  expect(page).to have_content(@book.content)
+  expect(page).to have_content(@book.weight)
+  expect(page).to have_content(@book.size)
+  expect(page).to have_content(@book.date)
 end
